337ee7df2044053145a09e1c35db90e1685807d7
K-Minho <alsgh9979@naver.com>
2023-02-12 19:26:12 +0900

    detail 완

---
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -0,0 +1,22 @@
+package shop.mtcoding.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import shop.mtcoding.blog.model.BoardRepository;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardRepository boardRepository;
+    
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable int id, Model model) {
+        model.addAttribute("boardDto", boardRepository.findByIdWithUser(id));
+        return "board/detail";
+    }
+}
---
+++ src/test/java/shop/mtcoding/blog/controller/BoardControllerTest.java
@@ -0,0 +1,45 @@
+package shop.mtcoding.blog.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import shop.mtcoding.blog.dto.board.BoardResp.BoardDetailRespDto;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class BoardControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+    
+    @Test
+    public void detail_test() throws Exception {
+        // given
+        int id = 1;
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/board/" + id));
+        Map<String, Object> map = resultActions.andReturn().getModelAndView().getModel();
+        BoardDetailRespDto boardDto = (BoardDetailRespDto) map.get("boardDto");
+
+        // then
+        resultActions.andExpect(status().isOk());
+        assertThat(boardDto.getUsername()).isEqualTo("ssar");
+        assertThat(boardDto.getUserId()).isEqualTo(1);
+        assertThat(boardDto.getTitle()).isEqualTo("제목1");
+    }
+}
